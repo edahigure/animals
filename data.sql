909b37143927028c5e17aa8d393132b2a6105233
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-04-05 16:21:48 -0600

    Create new tables owners, species and do queries

--- data.sql
+++ data.sql
@@ -15,16 +15,16 @@ VALUES ( 'Devimon', '2017-05-12', 5 , true, 11.0);
 
 
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
-VALUES ( 'Charmander', '2020-02-08', 0 , false , -11.0);
+VALUES ( 'Charmander', '2020-02-08', 0 , false , 11.0);
 
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
-VALUES ( 'Plantmon', '2021-11-15', 2 , true , -5.7);
+VALUES ( 'Plantmon', '2021-11-15', 2 , true , 5.7);
 
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
-VALUES ( 'Squirtle', '1993-04-02', 3 , false , -12.13);
+VALUES ( 'Squirtle', '1993-04-02', 3 , false , 12.13);
 
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
-VALUES ( 'Angemon', '2005-06-12', 1 , true , -45.0);
+VALUES ( 'Angemon', '2005-06-12', 1 , true , 45.0);
 
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES ( 'Boarmon', '2005-06-07', 7 , true , 20.4);
@@ -32,8 +32,79 @@ VALUES ( 'Boarmon', '2005-06-07', 7 , true , 20.4);
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES ( 'Blossom', '1998-10-13', 3 , true , 17.0);
 
+
+
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)
 VALUES ( 'Ditto', '2022-05-14', 4 , true , 22.0);
+
+
+
+
+INSERT INTO owners (full_name,age)
+VALUES ( 'Sam Smith',  24);
+
+INSERT INTO owners (full_name,age)
+VALUES ( 'Jennifer Orwell',  19);
+
+INSERT INTO owners (full_name,age)
+VALUES ( 'Bob',  45);
+
+INSERT INTO owners (full_name,age)
+VALUES ( 'Melody Pond',  77);
+
+INSERT INTO owners (full_name,age)
+VALUES ( 'Dean Winchester',  14);
+
+INSERT INTO owners (full_name,age)
+VALUES ( 'Jodie Whittaker',  38);
+
+
+INSERT INTO species (name)
+VALUES ( 'Pokemon');
+
+INSERT INTO species (name)
+VALUES ( 'Digimon');
+
+
+
+
+BEGIN TRANSACTION;
+
+UPDATE animals
+SET species_id = 2
+WHERE  name LIKE '%mon';
+
+UPDATE animals
+SET species_id = 1
+WHERE  name NOT LIKE '%mon';
+
+COMMIT TRANSACTION;
+
+
+
+BEGIN TRANSACTION;
+
+UPDATE animals
+SET owners_id = 1
+WHERE  name = 'Agumon';
+
+UPDATE animals
+SET owners_id = 2
+WHERE  name IN ( 'Gabumon', 'Pikachu' );
+
+UPDATE animals
+SET owners_id = 3
+WHERE  name  IN ( 'Devimon', 'Plantmon');
+
+UPDATE animals
+SET owners_id = 4
+WHERE  name IN ( 'Charmander', 'Squirtle', 'Blossom' );
+
+UPDATE animals
+SET owners_id = 5
+WHERE  name IN ('Angemon', 'Boarmon');
+
+COMMIT TRANSACTION;
 
 
 
--- schema.sql
+++ schema.sql
@@ -7,9 +7,43 @@ CREATE TABLE animals(
   escape_attempts    INT,
   neutered     boolean,
   weight_kg    REAL,
+  species_id INT references species(id),
+  owners_id INT references owners(id),
+  CONSTRAINT fk_species
+      FOREIGN KEY(species_id) 
+	  REFERENCES species(id)
+      ON DELETE SET NULL,
+  CONSTRAINT fk_owners
+      FOREIGN KEY(owners_id) 
+	  REFERENCES owners(id)
+      ON DELETE SET NULL,
   PRIMARY KEY(id)
 );
 
+CREATE TABLE owners(
+  id            INT GENERATED ALWAYS AS IDENTITY,
+  full_name    		VARCHAR(100),
+  age               INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+  id            INT GENERATED ALWAYS AS IDENTITY,
+  name    		VARCHAR(100),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE result(
+    id            INT GENERATED ALWAYS AS IDENTITY,
+    full_name    VARCHAR(100),
+    num          INT,
+    PRIMARY KEY(id)
+);
+
+
+ALTER TABLE animals 
+   ADD FOREIGN KEY (species_id) REFERENCES species (id);
+
+
 ALTER TABLE animals
 ADD species varchar(255);
-
